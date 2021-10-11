eaf8389fffc94a7ebd6f6f7e24e6670a8bb8e817
valentina cantarelli <valentinacanterina@gmail.com>
2021-10-11 15:11:05 +0200

    aggiunta hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="hero">
     <section>
+      <i class="fas fa-chevron-left"></i>
       <div class="heroLeft"><span>  Devotion that never</span> <span class="alternativeTxt"><em>ends</em></span>
       <p>Lorem ipsum dmet,  uptatibus pro sint aut dolorum, aliquid 
         conquatur sit accusamus doloribus.</p>
-        <button>READ MORE</button>
+        <button><a href="#"> READ MORE</a></button>
       </div>
-      <div class="heroRight"></div>
+      <div class="heroRight">
+        <img src="../assets/img/h-2-slider-img-13.png" alt="foglia" class="bgimg">
+        <img src="../assets/img/h-2-slider-img-15.png" alt="hero">
+        <img src="../assets/img/h-2-slider-img-16.png" alt="hero">
+        <img src="../assets/img/h-2-slider-img-17.png" alt="foglia" class="bgimg">
+      </div>
+      <i class="fas fa-chevron-right"></i>
     </section>
-    
   </div>
 </template>
 
@@ -22,33 +28,63 @@ export default {
 @import '../assets/style/variables';
 .hero{
   width:100%;
-  height :200px;
+  height:200px;
   padding-top:80px;
 
-  .heroLeft{
-    width: 31%;
-    font-family: 'Poppins', sans-serif;
-    font-size:45px;
-    font-weight: 300;
-    line-height: 50px;
+  section{
+    display: flex;
+    width: 95%;
+    justify-content: space-between;
 
-    .alternativeTxt{
-      font-family: 'Cormorant Upright', serif;
-      font-size:65px;
-      font-weight: 700;
+    i{
+    background-color: $primaryCol;
+    height: 20px;
+    width:20px;
+    border-radius: 50%;
+    color:white;
+    margin: 90px 0px;
+    text-align: center;
+    line-height: 20px;
     }
-    p{
-      font-size: 13px;
-      line-height: 16px;
-      padding: 17px 0;
+
+    .heroLeft{
+      width: 30%;
+      font-family: 'Poppins', sans-serif;
+      font-size:45px;
+      font-weight: 300;
+      line-height: 50px;
+
+      .alternativeTxt{
+        font-family: 'Cormorant Upright', serif;
+        font-size:65px;
+        font-weight: 700;
+      }
+      p{
+        font-size: 13px;
+        line-height: 16px;
+        padding: 17px 0;
+      }
+      button{
+        font-size:12px;
+        border: 3px solid $primaryCol;
+        padding: 8px 13px;
+        background-color: white;
+        font-weight: 700;
+      }
     }
-    button{
-      font-size:12px;
-      border: 3px solid $primaryCol;
-      padding: 8px 13px;
-      background-color: white;
-      font-weight: 700;
+    .heroRight{
+      width:39%;
+
+      img{
+        height: 250px;
+      }
+      .bgimg{
+        height:55px;
+      }
     }
-  }
-}
+  }  
+}  
+
+  
+
 </style>
