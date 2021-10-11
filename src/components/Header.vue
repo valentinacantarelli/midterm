3f5643a07c9227c6c6ebe7bfe298c816592afcdc
valentina cantarelli <valentinacanterina@gmail.com>
2021-10-11 12:10:22 +0200

    aggiunto header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,81 @@
 <template>
-  <div></div>
+  <header>
+    <img src="../assets/img/logo-sidearea-1.png" alt="logo">
+    <nav>
+      <ul>
+        <li v-for="(el,index) in menu" :key="index">
+          <a :href="el.url">{{el.text}}</a></li>
+          <i class="fas fa-th"></i>
+      </ul>
+      
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
-    name:Header
+    name:"Header",
+    data(){
+      return{
+        menu:[
+          {
+            text:"HOME",
+            url:"#",
+          },
+          {
+            text:"PAGES",
+            url:"#",
+          },
+          {
+            text:"PORTFOLIO",
+            url:"#",
+          },
+          {
+            text:"BLOG",
+            url:"#",
+          },
+          {
+            text:"SHOP",
+            url:"#",
+          },
+          {
+            text:"ELEMENTS",
+            url:"#",
+          },
+        ]
+      }
+    },
 }
 </script>
 
 <style  lang="scss" scope>
+header{
+  display: flex;
+  width:100%;
+  padding:20px;
+  justify-content: space-between;
+  font-family: 'Poppins', sans-serif;
 
+  img{
+    height: 20px;
+  }
+
+  ul{
+    display: flex;
+    margin-right:18px;
+
+    li{
+      padding:0px 15px;
+      font-size: 13px;
+      font-weight:500;
+
+      a:hover{
+        border-bottom: 3px solid #f0cebe;
+      }
+    }
+  }
+  .fa-th{
+    padding:2px 0 0 16px;
+  }
+}
 </style>
